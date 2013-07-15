5898cffa09959e939db744f7eb2b76b39281abf8
Gant <GantMan@gmail.com>
2013-07-15 13:43:29 -0500

    Tests fixed

--- spec/root_screen_spec.rb
+++ spec/root_screen_spec.rb
@@ -1,13 +1,18 @@
 describe "The root screen" do
   tests RootScreen
 
+  def controller
+    @controller ||= RootScreen.new
+    @controller.on_presented # force loading the view
+    @controller
+  end
+
   it "loads the controller fine" do
     controller.should.not == nil
   end
 
   it "has the necessary buttons" do
-    # Why the hell is this nil in test but comes back fine in running?
-    fbutton = controller.instance_variable_get("@face_pictures_button").titleLabel.text
-    p fbutton
+    fbutton = controller.instance_variable_get("@face_pictures_button").should.not == nil
+    # check .titleLabel.text?
   end
 end
